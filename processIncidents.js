1cc0c58bad4e7792b64c4678a9a27b830c874822
jpbatz <jph@hawaii.edu>
2015-02-07 20:03:29 -1000

    MBA refactored time conversion to a function

--- processIncidents.js
+++ processIncidents.js
@@ -1,47 +1,78 @@
 // processIncidents.js
 // processes each incident in incidents JSON object
+// ================================================
+var bodyParser = require('body-parser');
+var request = require('request');
 
 // calls:
+//  getIncidentData.js
 //  processAddress.js
 //  getGeoCode.js
 //  storeIncident.js
 
 // ================================================
-var bodyParser = require('body-parser');
-var request = require('request');
 
-// getIncidentData.js to get JSON data from Traffic API
+// (A) getIncidentData.js to get JSON data from Traffic API
 
-// process for each incident (this file)
+// (J) process for each incident (this file)
 
-  //    convert UTC/EPOC to local date/time
-  var utcSeconds = 1234567890;
-  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-  d.setUTCSeconds(utcSeconds);
-  // d holds the date in your local timezone
-  console.log("Local Date/Time is: " + d);
+  // var epocDateTime = ... // pull time from JSON API date
+  var localTime = convertEpocToLocalTime(1234567890);
+  console.log(localTime);
 
-  //    validate address with processAddress.js
+  // validate address with processAddress.js
     // blank location
     // remove X?
-  var addr = "Sydney,Australia";
 
-  //    get geo code from GeoCode API with getGeoCode.js
-  // { "results":[
-  //    { "locations":[
-  //      { "latLng":
-  //        { "lng":-76.305669,
-  //          "lat":40.03813},
+  var addr = "Antarctica";
 
-  var request = require('request');
+  // var geo_coord = getGeoCode(addr);
+  // console.log(geo_coord);
+
+  // console.log("Lat = " + geo_coord.lat);
+  // console.log("Lng = " + geo_coord.lng);
+  
+  // get geo coordinates from GeoCode API
   request('http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluu8216zl1%2Cax%3Do5-94250w&location='+addr, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       // console.log(body); // Show the HTML for the Google homepage.
       var jsonData = JSON.parse(body);
       console.log(jsonData.results[0].locations[0].latLng.lng);
       console.log(jsonData.results[0].locations[0].latLng.lat);
-      // console.log(body.results);
     }
   });
 
-  //    store incident to db storeIncident.js
+  //    (A) store incident to db storeIncident.js
+
+
+// Helper Functions
+// ================
+
+function convertEpocToLocalTime(utcSeconds) {
+  // convert UTC/EPOC to local date/time
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(utcSeconds);
+  // d holds the date in your local timezone
+  // Todo: remove GMT-1000 (HST)
+  return d;
+}
+
+// no work!
+// function getGeoCode(addr) {
+//   // var geo_data;
+//   // var jsonData;
+//   request('http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluu8216zl1%2Cax%3Do5-94250w&location='+addr, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       // console.log(body); // Show the HTML for the Google homepage.
+//       jsonData = JSON.parse(body);
+//       // console.log(jsonData.results[0].locations[0].latLng.lng);
+//       // console.log(jsonData.results[0].locations[0].latLng.lat);
+//       geo_data = {
+//         "lng": jsonData.results[0].locations[0].latLng.lng, 
+//         "lat": jsonData.results[0].locations[0].latLng.lat
+//       };
+//     }
+//   });
+//   console.log(geo_data);
+//   return geo_data;
+// }
