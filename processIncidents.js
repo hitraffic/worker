c1dff5bdd839fb2d2ea199caec1d713ae63b785e
jpbatz <jph@hawaii.edu>
2015-02-08 12:54:41 -1000

    MBA removes key in processIndcident.js and replaces it with new one in config.json, clean up code and comments

--- processIncidents.js
+++ processIncidents.js
@@ -1,69 +1,84 @@
 // processIncidents.js
 // processes each incident in incidents JSON object
+//    -converts stored epoch date to human readable local format
+//    -validates/corrects stored address idiosyncracies for geo code retrieval
+//    -inserts incident record (complete with converted date/time and additional lat/lng geo coordinates) to db
+//    -data to be retrieved by mid and front end devs
+// 
+// calls:
+//  getIncidentData.js - retrieves JSON data from Traffic API
+//  processAddress.js - validates faulty address entry for GeoCode API
+//  getGeoCode.js - uses valid to retrieve geo coordinates from GeoCode API
+//  storeIncident.js - stores incident record with geo coordinates in PostgreSQL db
+//
 // ================================================
 var request = require('request');
+var config = require('./config.json');
+// var getIncidents = require('./getIncidentData.js');
 
-// calls:
-//  getIncidentData.js
-//  processAddress.js
-//  getGeoCode.js
-//  storeIncident.js
+// [1] (A) getIncidentData.js to get JSON data from Traffic API
 
-// ================================================
+// var incidents = getIncidents();
 
-// (A) getIncidentData.js to get JSON data from Traffic API
+// [2] (J) process for each incident (this file)
 
-// (J) process for each incident (this file)
-
+  // [2a] convert stored epoch time to local time
   // var epochDateTime = ... // pull time from JSON API date
-  var localTime = convertEpochToLocalTime(1234567890);
-  console.log(localTime);
+  // var localTime = convertEpochToLocalTime(epochDateTime);
+  var localTime = convertEpochToLocalTime(0123456789);
+  console.log(localTime);  // sanity check
+  // need to change incident date
 
-  // validate address with processAddress.js:
-    // blank location
-    // remove X?
-    // add space before and after "&"
+  // [2b] validate address with processAddress.js (possible issues):
+    // blank location - assign to null
+    // remove X's? - probably not necessary
+    // add space before and after "&" - required
 
-  var addr = "Antarctica";
+  var addr = "Antarctica";  // actual address to be extracted from JSON incident
 
+  // [2c] get geo coordinates from GeoCode API
+
+  // cleaner, but does not work:
   // var geo_coord = getGeoCode(addr);
   // console.log(geo_coord);
 
   // console.log("Lat = " + geo_coord.lat);
   // console.log("Lng = " + geo_coord.lng);
   
-  // get geo coordinates from GeoCode API
-  request('http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluu8216zl1%2Cax%3Do5-94250w&location='+addr, function (error, response, body) {
+  // not so clean, but works:
+  request('http://open.mapquestapi.com/geocoding/v1/address?key='+config.AppKey+'&location='+addr, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // console.log(body); // Show the HTML for the Google homepage.
+      // console.log(body); // sanity check
       var jsonData = JSON.parse(body);
-      console.log(jsonData.results[0].locations[0].latLng.lng);
-      console.log(jsonData.results[0].locations[0].latLng.lat);
+      // console.log(jsonData); // sanity check
+      console.log("lng: " + jsonData.results[0].locations[0].latLng.lng);
+      console.log("lat: " + jsonData.results[0].locations[0].latLng.lat);
     }
   });
 
-  // (A) store incident to db storeIncident.js
+  // [2d] (A) store incident to db storeIncident.js
+
+// (J) end [2] processing each incident (this file)
 
 
 // Helper Functions
 // ================
 
+// convert UTC/EPOCH to local date/time
+// TODO: remove GMT-1000 (HST)
 function convertEpochToLocalTime(utcSeconds) {
-  // convert UTC/EPOCH to local date/time
-  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  var d = new Date(0); // 0 is the key which sets the date to the epoch
   d.setUTCSeconds(utcSeconds);
-  // d holds the date in your local timezone
-  // Todo: remove GMT-1000 (HST)
-  return d;
+  return d;  // d holds the date in your local timezone
 }
 
-// no work!
+// preferred, but does not work - object not accessible (undefined) in main space:
 // function getGeoCode(addr) {
 //   // var geo_data;
 //   // var jsonData;
-//   request('http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluu8216zl1%2Cax%3Do5-94250w&location='+addr, function (error, response, body) {
+//   request('http://open.mapquestapi.com/geocoding/v1/address?key='+config.AppKey+'&location='+addr, function (error, response, body) {
 //     if (!error && response.statusCode == 200) {
-//       // console.log(body); // Show the HTML for the Google homepage.
+//       // console.log(body); // sanity check
 //       jsonData = JSON.parse(body);
 //       // console.log(jsonData.results[0].locations[0].latLng.lng);
 //       // console.log(jsonData.results[0].locations[0].latLng.lat);
@@ -73,6 +88,6 @@ function convertEpochToLocalTime(utcSeconds) {
 //       };
 //     }
 //   });
-//   console.log(geo_data);
+//   // console.log(geo_data);
 //   return geo_data;
 // }
