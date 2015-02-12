05abba6a77a37e2a6e25b880a0ff768299d8f837
Andrew <champstarr@gmail.com>
2015-02-11 16:14:27 -1000

    creating offset function to return all incidents

--- getIncidentData.js
+++ getIncidentData.js
@@ -2,9 +2,24 @@ var request = require('request');
 // getIncidentData.js simply return the object.
 request('https://data.honolulu.gov/resource/ix32-iw26.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body);
+    console.log(JSON.parse(body)[999]);
   }
 });
+
+function sendRequest (){//Take in parameters: $offset and $limit
+  request('https://data.honolulu.gov/resource/ix32-iw26.json?', handleResult);
+}
+
+function handleResult(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var dataEnries = JSON.parse(body); 
+    console.log(JSON.parse(body)[999]);
+  }
+}
+
+
+
+  )
 
 // retrieves JSON data from Traffic API
 // Use request method from "Postit"?
