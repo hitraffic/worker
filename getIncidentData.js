a50f9033dc276124a1941ce82e5c0b87fba37b8e
Andrew <champstarr@gmail.com>
2015-02-21 15:49:44 -1000

    iterating through records

--- getIncidentData.js
+++ getIncidentData.js
@@ -19,12 +19,22 @@ sequelize
 request('https://data.honolulu.gov/api/views/ix32-iw26/rows.json?accessType=DOWNLOAD', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var jsonData = JSON.parse(body); 
-     var dataEntries = jsonData.data;
-     console.log(dataEntries);
-    //  console.log(JSON.parse(body));
-     //console.log(dataEntries.data[0][12]);
-     console.log(dataEntries.data.length);
-    // dataEntries.forEach(function(item){
+     console.log(jsonData);
+     //console.log(jsonData.data[0][12]);
+     console.log(jsonData.data.length);
+     var dataLength = jsonData.data.length;
+     for (var i = 0; i < dataLength; i++) {
+        console.log("item: "+jsonData.data[i][0]);
+        console.log("date: "+jsonData.data[i][8]);
+        console.log("code: "+jsonData.data[i][9]);
+        console.log("type: "+jsonData.data[i][10]);
+        console.log("address: "+jsonData.data[i][11]);
+        console.log("location: "+jsonData.data[i][12]);
+        console.log("area: "+jsonData.data[i][13]); 
+        console.log("****");     
+     };
+
+    // jsonData.forEach(function(item){
     //   console.log(item[0][0]);
       // console.log(item.address);
       // console.log(item.code);
