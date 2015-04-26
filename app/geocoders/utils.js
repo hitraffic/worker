2a1039ace17f254ff204fbe721452f0df14be1c6
George Lee <keoki.lee@gmail.com>
2015-04-25 21:09:46 -1000

    Fix bug with Kamehameha Highway.

--- app/geocoders/utils.js
+++ app/geocoders/utils.js
@@ -1,8 +1,8 @@
-const KAMEHAMEHA_REGEX = /\sKAM\s/g;
+const KAMEHAMEHA_REGEX = /KAM\sHWY/g;
 
 function processAddress(address) {
   // Check if address contains "KAM"
-  address = address.replace(KAMEHAMEHA_REGEX, " KAMEHAMEHA ");
+  address = address.replace(KAMEHAMEHA_REGEX, " KAMEHAMEHA HWY");
   return address;
 }
 
