da1a94b59815adefd1458de110722392246d9680
George Lee <keoki.lee@gmail.com>
2015-04-25 21:20:46 -1000

    Remove unnecessary space.

--- app/geocoders/utils.js
+++ app/geocoders/utils.js
@@ -2,7 +2,7 @@ const KAMEHAMEHA_REGEX = /KAM\sHWY/g;
 
 function processAddress(address) {
   // Check if address contains "KAM"
-  address = address.replace(KAMEHAMEHA_REGEX, " KAMEHAMEHA HWY");
+  address = address.replace(KAMEHAMEHA_REGEX, "KAMEHAMEHA HWY");
   return address;
 }
 
